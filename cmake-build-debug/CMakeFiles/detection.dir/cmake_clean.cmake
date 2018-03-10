b4f29bd12ae6bd2ebad5261e5ef99be083c5e3ef
yellowos <2442605351@qq.com>
2018-03-10 19:09:37 +0800

    fix problem

---
+++ cmake-build-debug/CMakeFiles/detection.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detection.dir/Main.cpp.o"
+  "CMakeFiles/detection.dir/Frame.cpp.o"
+  "CMakeFiles/detection.dir/Capture.cpp.o"
+  "detection.pdb"
+  "detection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
